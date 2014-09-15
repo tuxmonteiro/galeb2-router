e55f4585deb6eb02c814d518825cba2737730676
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-09-14 21:52:41 -0300

    Added HttpCode.Accepted

--- src/main/java/com/globo/galeb/core/HttpCode.java
+++ src/main/java/com/globo/galeb/core/HttpCode.java
@@ -5,13 +5,14 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 
 public class HttpCode {
 
-    public static int Ok                  = HttpResponseStatus.OK.code(); // 200
-    public static int TemporaryRedirect   = HttpResponseStatus.TEMPORARY_REDIRECT.code(); // 302
-    public static int BadRequest          = HttpResponseStatus.BAD_REQUEST.code(); // 400
-    public static int MethotNotAllowed    = HttpResponseStatus.METHOD_NOT_ALLOWED.code(); // 405
-    public static int InternalServerError = HttpResponseStatus.INTERNAL_SERVER_ERROR.code(); // 500
-    public static int BadGateway          = HttpResponseStatus.BAD_GATEWAY.code(); // 502
-    public static int GatewayTimeout      = HttpResponseStatus.GATEWAY_TIMEOUT.code(); // 504
+    public static final int Ok                  = HttpResponseStatus.OK.code(); // 200
+    public static final int Accepted            = HttpResponseStatus.ACCEPTED.code(); // 202
+    public static final int TemporaryRedirect   = HttpResponseStatus.TEMPORARY_REDIRECT.code(); // 302
+    public static final int BadRequest          = HttpResponseStatus.BAD_REQUEST.code(); // 400
+    public static final int MethotNotAllowed    = HttpResponseStatus.METHOD_NOT_ALLOWED.code(); // 405
+    public static final int InternalServerError = HttpResponseStatus.INTERNAL_SERVER_ERROR.code(); // 500
+    public static final int BadGateway          = HttpResponseStatus.BAD_GATEWAY.code(); // 502
+    public static final int GatewayTimeout      = HttpResponseStatus.GATEWAY_TIMEOUT.code(); // 504
 
     private HttpCode() {
     }
