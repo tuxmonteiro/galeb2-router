259d61e6beac06dba5f7263db31d0f7d8bdbddf1
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-09-16 20:13:03 -0300

    Included HttpCode.NotFound

--- src/main/java/com/globo/galeb/core/HttpCode.java
+++ src/main/java/com/globo/galeb/core/HttpCode.java
@@ -10,6 +10,7 @@ public class HttpCode {
     public static final int Found               = HttpResponseStatus.FOUND.code(); // 302
     public static final int TemporaryRedirect   = HttpResponseStatus.TEMPORARY_REDIRECT.code(); // 307
     public static final int BadRequest          = HttpResponseStatus.BAD_REQUEST.code(); // 400
+    public static final int NotFound            = HttpResponseStatus.NOT_FOUND.code(); // 404
     public static final int MethotNotAllowed    = HttpResponseStatus.METHOD_NOT_ALLOWED.code(); // 405
     public static final int InternalServerError = HttpResponseStatus.INTERNAL_SERVER_ERROR.code(); // 500
     public static final int BadGateway          = HttpResponseStatus.BAD_GATEWAY.code(); // 502
