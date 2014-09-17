ab34b85878cd4f87c62678fe62c97685865a22cc
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-09-17 05:51:42 -0300

    ManagerService and GetMatcherHandler class added

---
+++ src/main/java/com/globo/galeb/core/ManagerService.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2014 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+package com.globo.galeb.core;
+
+import java.io.UnsupportedEncodingException;
+import java.util.EnumSet;
+
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.DecodeException;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+
+public class ManagerService {
+
+    public enum UriSupported {
+        VIRTUALHOST,
+        BACKEND,
+        FARM,
+        VERSION
+    }
+
+    private String id;
+    private final Logger log;
+    private HttpServerRequest req = null;
+    private ServerResponse serverResponse = null;
+
+    public ManagerService(String id, final Logger log) {
+        this.id = id;
+        this.log = log;
+    }
+
+    public ManagerService setRequest(final HttpServerRequest req) {
+        this.req = req;
+        return this;
+    }
+
+    public ManagerService setResponse(final ServerResponse resp) {
+        this.serverResponse = resp;
+        return this;
+    }
+
+    public boolean checkUriOk() {
+        String uriBase = "";
+        try {
+            if (req.params() != null) {
+                uriBase =  req.params().contains("uriBase") ? req.params().get("uriBase") :
+                    req.params().contains("param0") ? req.params().get("param0") : "";
+                uriBase = java.net.URLDecoder.decode(uriBase, "UTF-8");
+            } else {
+                log.error("UriBase is null");
+                endResponse(HttpCode.NotFound, "UriBase is null");
+                return false;
+            }
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage());
+            endResponse(HttpCode.NotFound, e.getMessage());
+            return false;
+        }
+        for (UriSupported uriEnum : EnumSet.allOf(UriSupported.class)) {
+            if (uriBase.equals(uriEnum.toString().toLowerCase())) {
+                return true;
+            }
+        }
+        endResponse(HttpCode.NotFound, String.format("URI /%s not supported", uriBase));
+        return false;
+    }
+
+    public boolean checkMethodOk(String methodId) {
+        String method = req.method();
+        if (!methodId.equalsIgnoreCase(method)) {
+            endResponse(HttpCode.MethotNotAllowed, "Method Not Allowed");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkIdConsistency(JsonObject entityJson, String idFromUri) {
+        String idFromJson = entityJson.getString(IJsonable.jsonIdFieldName, "");
+        if ("".equals(idFromJson) || "".equals(idFromUri) || !idFromJson.equals(idFromUri)) {
+            endResponse(HttpCode.BadRequest,
+                    String.format("IDs inconsistents: bodyId(%s) not equal uriId(%s)", idFromJson, idFromUri));
+            return false;
+        }
+        return true;
+    }
+
+    public String getRequestId() {
+        String id = "";
+        try {
+            id = req.params() != null && req.params().contains(IJsonable.jsonIdFieldName) ?
+                    java.net.URLDecoder.decode(req.params().get(IJsonable.jsonIdFieldName), "UTF-8") : "";
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage());
+        }
+        return id;
+    }
+
+
+    public boolean checkIdPresent() {
+        String id = getRequestId();
+
+        if ("".equals(id)) {
+            endResponse(HttpCode.BadRequest, String.format("ID absent", id));
+            return false;
+        }
+        return true;
+    }
+
+    public boolean endResponse(int statusCode, String message) {
+        if (statusCode < HttpCode.BadRequest) {
+            log.info(message);
+        } else {
+            log.warn(message);
+        }
+        boolean isOk = true;
+        try {
+            serverResponse.setStatusCode(statusCode)
+                .setMessage(HttpCode.getMessage(statusCode, true))
+                .setId(id)
+                .end();
+        } catch (RuntimeException e) {
+            log.error(e.getMessage());
+            isOk = false;
+        }
+        return isOk;
+    }
+
+    public int statusFromMessageSchema(String message, String uri, boolean registerLog) {
+
+        SafeJsonObject json = new SafeJsonObject(message);
+        if ("{}".equals(json.encode())) {
+            if (registerLog) log.error(String.format("Json decode error: %s", message));
+            return HttpCode.BadRequest;
+        }
+
+        if (!json.containsField("version")) {
+            if (registerLog) log.error(String.format("Version is mandatory: %s", message));
+            return HttpCode.BadRequest;
+        }
+
+        if (!uri.startsWith("/farm")) {
+            if (!json.containsField(IJsonable.jsonIdFieldName)) {
+                if (registerLog) log.error(String.format("ID is mandatory: %s", message));
+                return HttpCode.BadRequest;
+            }
+        } else {
+            SafeJsonObject jsonRoutes = new SafeJsonObject();
+
+            boolean hasVistualhosts = true;
+            if (json.containsField("virtualhosts")) {
+                try {
+                    jsonRoutes = json.getJsonArray("virtualhosts");
+                } catch (DecodeException itsNotArray) {
+                    hasVistualhosts = false;
+                }
+            } else {
+                hasVistualhosts = false;
+            }
+
+            if (!hasVistualhosts) {
+                if (registerLog) log.error(String.format("Reading virtualhosts failed: %s", message));
+                return HttpCode.BadRequest;
+            }
+
+            for (Object routeObj: jsonRoutes.toList()) {
+                SafeJsonObject routeJson = (SafeJsonObject) routeObj;
+                if (!routeJson.containsField(IJsonable.jsonIdFieldName)) {
+                    if (registerLog) log.error(String.format("ID not found: %s", routeJson.toString()));
+                    return HttpCode.BadRequest;
+                } else {
+                    String key = routeJson.getString(IJsonable.jsonIdFieldName);
+                    if ("".equals(key)) {
+                        if (registerLog) log.error(String.format("ID is invalid: %s", routeJson.toString()));
+                        return HttpCode.BadRequest;
+                    }
+                }
+            }
+        }
+        return HttpCode.Ok;
+    }
+
+    public int statusFromMessageSchema(String message, String uri) {
+        return statusFromMessageSchema(message, uri, true);
+    }
+
+}
---
+++ src/main/java/com/globo/galeb/handlers/manager/GetMatcherHandler.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2014 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+package com.globo.galeb.handlers.manager;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpHeaders;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+
+import com.globo.galeb.core.Farm;
+import com.globo.galeb.core.HttpCode;
+import com.globo.galeb.core.ManagerService;
+import com.globo.galeb.core.Serializable;
+import com.globo.galeb.core.Server;
+import com.globo.galeb.core.ServerResponse;
+
+public class GetMatcherHandler implements Handler<HttpServerRequest> {
+
+    private final Logger log;
+    private String httpServerName = null;
+    private final Farm farm;
+    private final String classId;
+
+    public GetMatcherHandler(String id, final Logger log, final Farm farm) {
+        this.log = log;
+        this.farm = farm;
+        this.classId = id;
+    }
+
+    @Override
+    public void handle(HttpServerRequest req) {
+        final ServerResponse serverResponse = new ServerResponse(req, log, null, false);
+        ManagerService managerService = new ManagerService(classId, log).setRequest(req).setResponse(serverResponse);
+
+        if (httpServerName==null) {
+            httpServerName = req.headers().contains(HttpHeaders.HOST) ? req.headers().get(HttpHeaders.HOST) : "SERVER";
+            Server.setHttpServerName(httpServerName);
+        }
+
+        if (!managerService.checkUriOk()) {
+            return;
+        }
+
+        String uriBase = "";
+        String id = "";
+        String message = "";
+
+        if (req.params()!=null) {
+            uriBase = req.params().contains("param0") ? req.params().get("param0") : "";
+            id = req.params().contains("param1") ? req.params().get("param1") : "";
+        }
+
+        try {
+            uriBase = java.net.URLDecoder.decode(uriBase, "UTF-8");
+            id = java.net.URLDecoder.decode(id, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            serverResponse.setStatusCode(HttpCode.BadRequest)
+                                .setId(id)
+                                .end();
+            log.error("Unsupported Encoding");
+            return;
+        }
+
+        switch (uriBase) {
+            case "version":
+                message = new JsonObject().putNumber("version", farm.getVersion()).encodePrettily();
+                break;
+            case "farm":
+                message = farm.toJson().encodePrettily();
+                break;
+            case "virtualhost":
+                message = getResult(id, farm.getVirtualhosts(), "virtualhosts");
+                break;
+            case "backend":
+                message = getResult(id, farm.getBackends(), "backends");
+                break;
+            default:
+                message = "";
+                break;
+        }
+
+        int statusCode = HttpCode.Ok;
+        if ("".equals(message)) {
+            statusCode = HttpCode.NotFound;
+            message = HttpCode.getMessage(statusCode, true);
+        }
+
+        serverResponse.setStatusCode(statusCode)
+            .setMessage(message)
+            .setId(id)
+            .end();
+        log.info(String.format("GET /%s", uriBase));
+    }
+
+    public String getResult(String key, Collection<? extends Serializable> collection, String arrayFieldName) {
+        String result = "";
+        boolean isArray = false;
+        JsonArray entityArray = new JsonArray();
+
+        for (Serializable entityObj: collection) {
+            entityArray.add(entityObj.toJson());
+            if (!"".equals(key)) {
+                if (entityObj.toString().equalsIgnoreCase(key)) {
+                    result = entityObj.toJson().encodePrettily();
+                    break;
+                }
+            } else {
+                isArray = true;
+            }
+        }
+        return !isArray ? result : new JsonObject().putArray(arrayFieldName, entityArray).encodePrettily();
+    }
+}
