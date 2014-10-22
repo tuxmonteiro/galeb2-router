cb3f9530bdf55dcbae7005bd048d1479ca4c3214
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-10-22 09:11:59 -0200

    JavaDoc: com.globo.galeb.consistenthash

--- src/main/java/com/globo/galeb/consistenthash/ConsistentHash.java
+++ src/main/java/com/globo/galeb/consistenthash/ConsistentHash.java
@@ -7,10 +7,11 @@
  *
  * Authors: See AUTHORS file
  *
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
- * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- * PARTICULAR PURPOSE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.globo.galeb.consistenthash;
 
@@ -18,12 +19,31 @@ import java.util.Collection;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+/**
+ * Class ConsistentHash.
+ *
+ * @param <T> the generic type of node
+ * @author: See AUTHORS file.
+ * @version: 1.0.0, 19/10/2014.
+ */
 public class ConsistentHash<T> {
 
+    /** The {@link HashAlgorithm Hash Algorithm} */
     private HashAlgorithm               hashAlgorithm;
+
+    /** The number of replicas. */
     private int                         numberOfReplicas;
+
+    /** The circle. */
     private final SortedMap<Integer, T> circle  = new TreeMap<Integer, T>();
 
+    /**
+     * Instantiates a new consistent hash.
+     *
+     * @param hashAlgorithm The {@link HashAlgorithm Hash Algorithm}
+     * @param numberOfReplicas the number of replicas
+     * @param nodes nodes collection
+     */
     public ConsistentHash(HashAlgorithm hashAlgorithm, int numberOfReplicas,
             Collection<T> nodes) {
         this.hashAlgorithm = hashAlgorithm;
@@ -34,18 +54,34 @@ public class ConsistentHash<T> {
         }
     }
 
+    /**
+     * Adds a node.
+     *
+     * @param node a node
+     */
     public void add(T node) {
         for (int i = 0; i < numberOfReplicas; i++) {
             circle.put(hashAlgorithm.hash(node.toString() + i), node);
         }
     }
 
+    /**
+     * Removes a node.
+     *
+     * @param node a node
+     */
     public void remove(T node) {
         for (int i = 0; i < numberOfReplicas; i++) {
             circle.remove(hashAlgorithm.hash(node.toString() + i));
         }
     }
 
+    /**
+     * Gets a node.
+     *
+     * @param key a key
+     * @return a node from cicle
+     */
     public T get(String key) {
         if (circle.isEmpty()) {
             return null;
@@ -60,6 +96,13 @@ public class ConsistentHash<T> {
         return circle.get(hash);
     }
 
+    /**
+     * Rebuild the cicle.
+     *
+     * @param hashAlgorithm The {@link HashAlgorithm Hash Algorithm}
+     * @param numberOfReplicas the number of replicas
+     * @param nodes nodes collection
+     */
     public void rebuild(HashAlgorithm hashAlgorithm, Integer numberOfReplicas,
             Collection<T> nodes) {
         if (hashAlgorithm!=null) {
--- src/main/java/com/globo/galeb/consistenthash/HashAlgorithm.java
+++ src/main/java/com/globo/galeb/consistenthash/HashAlgorithm.java
@@ -5,7 +5,7 @@
  * This source is subject to the Apache License, Version 2.0.
  * Please see the LICENSE file for more information.
  *
- * Authors: See AUTHORS file
+ * Authors: see AUTHORS file
  *
  * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
@@ -24,21 +24,46 @@ import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HashAlgorithm.
+ *
+ * @author: See AUTHORS file.
+ * @version: 1.0.0, 19/10/2014.
+ */
 public class HashAlgorithm {
 
+    /**
+     * The Enum HashType.
+     *
+     * @author: See AUTHORS file.
+     * @version: 1.0.0, 19/10/2014.
+     */
     public static enum HashType {
-        MD5,            // It's not so bad, but is a little slow.
-        MURMUR3_128,    // Slow
-        MURMUR3_32,     // Fast and reliable, but not so good for small keys
-        GOOD_FAST_32,   // Super Fast, but with excessive collisions. Why this was released?
-        ADLER_32,       // Unreliable
-        CRC_32,         // Unreliable
-        SHA1,           // Slow and Unreliable
-        SHA256,         // Reliable. Its a little slow, but not quite.
-        SHA512,         // Reliable, but very slow
-        SIP24           // Fast and reliable. The best for small keys
+
+        /** It's not so bad, but is a little slow. */
+        MD5,
+        /** Slow. */
+        MURMUR3_128,
+        /** Fast and reliable, but not so good for small keys. */
+        MURMUR3_32,
+        /** Super Fast, but with excessive collisions. Why this was released? */
+        GOOD_FAST_32,
+        /** Unreliable. */
+        ADLER_32,
+        /** Unreliable. */
+        CRC_32,
+        /** Slow and Unreliable. */
+        SHA1,
+        /** Reliable. Its a little slow, but not quite. */
+        SHA256,
+        /** Reliable, but very slow. */
+        SHA512,
+        /** Fast and reliable. The best for small keys. */
+        SIP24
     }
 
+    /** The Constant HASH_TYPE_MAP. */
     private static final Map<String, HashType> HASH_TYPE_MAP = new HashMap<>();
     static {
         for (HashType hash : EnumSet.allOf(HashType.class)) {
@@ -46,21 +71,41 @@ public class HashAlgorithm {
         }
     }
 
+    /** The hash type. */
     private final HashType hashType;
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     @Override
     public String toString() {
         return String.format("%s - hashType:%s", HashAlgorithm.class.getName(), hashType);
     }
 
+    /**
+     * Instantiates a new hash algorithm.
+     *
+     * @param hashType the hash type
+     */
     public HashAlgorithm(HashType hashType) {
         this.hashType = hashType;
     }
 
+    /**
+     * Instantiates a new hash algorithm.
+     *
+     * @param hashTypeStr the hash type str
+     */
     public HashAlgorithm(String hashTypeStr) {
         this.hashType = HASH_TYPE_MAP.containsKey(hashTypeStr) ? HashType.valueOf(hashTypeStr) : HashType.SIP24;
     }
 
+    /**
+     * Hash.
+     *
+     * @param key the key
+     * @return the int
+     */
     public int hash(Object key) {
         HashCode hashCode;
         HashFunction hashAlgorithm;
