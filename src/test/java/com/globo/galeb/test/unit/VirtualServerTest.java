275ead7a704e0eac7796b6389b98aac966fff6d0
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-11-10 19:15:46 -0200

    VirtualServerTest updated

--- src/test/java/com/globo/galeb/test/unit/VirtualServerTest.java
+++ src/test/java/com/globo/galeb/test/unit/VirtualServerTest.java
@@ -2,27 +2,34 @@ package com.globo.galeb.test.unit;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.*;
 
 import org.junit.Test;
 import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
 
+import com.globo.galeb.core.Farm;
 import com.globo.galeb.core.VirtualServer;
+import com.globo.galeb.core.bus.IQueueService;
 import com.globo.galeb.core.entity.IJsonable;
+import com.globo.galeb.metrics.ICounter;
 import com.globo.galeb.rules.Rule;
 
 public class VirtualServerTest {
 
+    VirtualServer virtualserver = new VirtualServer();
+
     @Test
     public void createWithDefaultContructor() {
-        VirtualServer virtualserver = new VirtualServer();
         assertThat(String.format("%s", virtualserver)).isEqualTo("UNDEF");
     }
 
     @Test
     public void createWithContructorWithId() {
         String id = "NOT_UNDEF";
-        VirtualServer virtualserver = new VirtualServer(id);
-        assertThat(String.format("%s", virtualserver)).isEqualTo(id);
+        VirtualServer virtualserver2 = new VirtualServer(id);
+        assertThat(String.format("%s", virtualserver2)).isEqualTo(id);
     }
 
     @Test
@@ -30,24 +37,24 @@ public class VirtualServerTest {
         String id = "NOT_UNDEF";
         JsonObject json = new JsonObject();
         json.putString(IJsonable.ID_FIELDNAME, id);
-        VirtualServer virtualserver = new VirtualServer(json);
+        VirtualServer virtualserver2 = new VirtualServer(json);
 
-        assertThat(String.format("%s", virtualserver)).isEqualTo(id);
+        assertThat(String.format("%s", virtualserver2)).isEqualTo(id);
     }
 
     @Test
     public void areEqualsIfIdIsTheSame() {
         String sameId = "sameId";
-        VirtualServer group1 = new VirtualServer(sameId);
-        VirtualServer group2 = new VirtualServer(sameId);
-        assertThat(group1).isEqualTo(group2);
+        VirtualServer virtualserver1 = new VirtualServer(sameId);
+        VirtualServer virtualserver2 = new VirtualServer(sameId);
+        assertThat(virtualserver1).isEqualTo(virtualserver2);
     }
 
     @Test
     public void areEqualsIfIdIsNotDefined() {
-        VirtualServer group1 = new VirtualServer();
-        VirtualServer group2 = new VirtualServer();
-        assertThat(group1).isEqualTo(group2);
+        VirtualServer virtualserver1 = new VirtualServer();
+        VirtualServer virtualserver2 = new VirtualServer();
+        assertThat(virtualserver1).isEqualTo(virtualserver2);
     }
 
     @Test
@@ -55,20 +62,28 @@ public class VirtualServerTest {
         String oneId = "oneId";
         String otherId = "otherId";
 
-        VirtualServer group1 = new VirtualServer(oneId);
-        VirtualServer group2 = new VirtualServer(otherId);
-        assertThat(group1).isNotEqualTo(group2);
+        VirtualServer virtualserver1 = new VirtualServer(oneId);
+        VirtualServer virtualserver2 = new VirtualServer(otherId);
+        assertThat(virtualserver1).isNotEqualTo(virtualserver2);
     }
 
     private boolean createRule(String ruleId, final VirtualServer virtualserver) {
         Rule rule = mock(Rule.class);
+
+        when(rule.setLogger((Logger) anyObject())).thenReturn(rule);
+        when(rule.setCounter((ICounter) anyObject())).thenReturn(rule);
+        when(rule.setQueueService((IQueueService) anyObject())).thenReturn(rule);
+        when(rule.setFarm((Farm) anyObject())).thenReturn(rule);
+        when(rule.setPlataform(anyObject())).thenReturn(rule);
+        when(rule.setStaticConf((String) anyObject())).thenReturn(rule);
+        when(rule.setStaticConf((JsonObject) anyObject())).thenReturn(rule);
+
         return virtualserver.addEntity(rule);
     }
 
     @Test
     public void addNewRule() {
         String ruleId = "newrule";
-        VirtualServer virtualserver = new VirtualServer();
         boolean ruleCreated = createRule(ruleId, virtualserver);
 
         assertThat(ruleCreated).isTrue();
@@ -78,7 +93,6 @@ public class VirtualServerTest {
     @Test
     public void addExistingRule() {
         String ruleId = "newrule";
-        VirtualServer virtualserver = new VirtualServer();
         createRule(ruleId, virtualserver);
         boolean ruleCreated = createRule(ruleId, virtualserver);
 
@@ -88,13 +102,21 @@ public class VirtualServerTest {
 
     private boolean removeRule(String ruleId, final VirtualServer virtualserver) {
         Rule rule = mock(Rule.class);
+
+        when(rule.setLogger((Logger) anyObject())).thenReturn(rule);
+        when(rule.setCounter((ICounter) anyObject())).thenReturn(rule);
+        when(rule.setQueueService((IQueueService) anyObject())).thenReturn(rule);
+        when(rule.setFarm((Farm) anyObject())).thenReturn(rule);
+        when(rule.setPlataform(anyObject())).thenReturn(rule);
+        when(rule.setStaticConf((String) anyObject())).thenReturn(rule);
+        when(rule.setStaticConf((JsonObject) anyObject())).thenReturn(rule);
+
         return virtualserver.removeEntity(rule);
     }
 
     @Test
     public void removeExistingRule() {
         String ruleId = "myrule";
-        VirtualServer virtualserver = new VirtualServer();
         createRule(ruleId, virtualserver);
         boolean ruleRemoved = removeRule(ruleId, virtualserver);
 
@@ -105,7 +127,6 @@ public class VirtualServerTest {
     @Test
     public void removeAbsentRule() {
         String ruleId = "myrule";
-        VirtualServer virtualserver = new VirtualServer();
         boolean ruleRemoved = removeRule(ruleId, virtualserver);
 
         assertThat(ruleRemoved).isFalse();
