fa81a8a2c178037149554156eb8d5d1e5e108e87
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-11-24 01:25:13 -0200

    Websocket support (draft)

---
+++ src/main/java/com/globo/galeb/handlers/BackendWebSocketHandler.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright (c) 2014 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.globo.galeb.handlers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VoidHandler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.core.http.WebSocket;
+import org.vertx.java.core.logging.Logger;
+
+import com.globo.galeb.bus.IQueueService;
+
+/**
+ * Class BackendWebSocketHandler.
+ *
+ * @author: See AUTHORS file.
+ * @version: 1.0.0, Oct 23, 2014.
+ */
+public class BackendWebSocketHandler implements Handler<WebSocket> {
+
+    /** The vertx. */
+    private final Vertx vertx;
+
+    /** The logger. */
+    private final Logger log;
+
+    /** The backend id. */
+    private final String backendId;
+
+    /** The server web socket. */
+    private final ServerWebSocket serverWebSocket;
+
+    /** Is frontend WebSocket closed? */
+    private boolean frontendWSisClosed;
+
+    /** Is backend WebSocker closed */
+    private boolean backendWSisClosed;
+
+    /** The messages queue. */
+    private final Queue<Buffer> messages = new LinkedList<Buffer>();
+
+    /** The websocket. */
+    private WebSocket websocket;
+
+    /** The initial request time. */
+    private Long initialRequestTime = null;
+
+    /**
+     * Instantiates a new backend web socket handler.
+     *
+     * @param vertx the vertx
+     * @param log the log
+     * @param backendId the backend id
+     * @param serverWebSocket the server web socket
+     */
+    public BackendWebSocketHandler(
+            final Vertx vertx,
+            final Logger log,
+            final String backendId,
+            final ServerWebSocket serverWebSocket) {
+        this.vertx = vertx;
+        this.backendId = backendId;
+        this.serverWebSocket = serverWebSocket;
+        this.log = log;
+    }
+
+    /* (non-Javadoc)
+     * @see org.vertx.java.core.Handler#handle(java.lang.Object)
+     */
+    @Override
+    public void handle(final WebSocket websocket) {
+
+        this.websocket = websocket;
+        websocket.exceptionHandler(new Handler<Throwable>() {
+            @Override
+            public void handle(Throwable event) {
+                log.error(String.format("backend: %s, message: %s", backendId, event.getMessage()));
+                vertx.eventBus().publish(IQueueService.QUEUE_HEALTHCHECK_FAIL, backendId );
+                websocket.close();
+            }
+        });
+
+        writeWebSocket(websocket, messages);
+
+        websocket.dataHandler(new Handler<Buffer>() {
+
+            @Override
+            public void handle(Buffer buffer) {
+                writeServerWebSocket(buffer);
+            }
+
+        });
+
+        websocket.closeHandler(new Handler<Void>() {
+
+            @Override
+            public void handle(Void event) {
+                backendWSisClosed = true;
+                log.debug("Backend WebSocket was closed");
+                if (! frontendWSisClosed) {
+                    frontendWSisClosed = true;
+                    serverWebSocket.close();
+                }
+            }
+        });
+    }
+
+    /**
+     * Write websocket.
+     *
+     * @param ws the websocket
+     * @param messages the messages queue
+     */
+    public void writeWebSocket(final WebSocket ws, final Queue<Buffer> messages) {
+        while (!messages.isEmpty()) {
+            writeWebSocket(ws, messages.poll());
+        }
+    }
+
+    /**
+     * Write websocket.
+     *
+     * @param ws the websocket
+     * @param message the message (buffer)
+     */
+    public void writeWebSocket(final WebSocket ws, Buffer message) {
+        if (!ws.writeQueueFull()) {
+            ws.write(message);
+        } else {
+            ws.pause();
+            ws.drainHandler(new VoidHandler() {
+                @Override
+                protected void handle() {
+                    ws.resume();
+                }
+            });
+        }
+
+    }
+
+    /**
+     * Write websocket (server side).
+     *
+     * @param buffer the buffer
+     */
+    public void writeServerWebSocket(Buffer buffer) {
+        if (serverWebSocket!=null) {
+            if (!serverWebSocket.writeQueueFull()) {
+                serverWebSocket.write(buffer);
+            } else {
+                serverWebSocket.pause();
+                serverWebSocket.drainHandler(new VoidHandler() {
+                    @Override
+                    protected void handle() {
+                        serverWebSocket.resume();
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * Gets the initial request time.
+     *
+     * @return the initial request time
+     */
+    public Long getInitialRequestTime() {
+        return initialRequestTime;
+    }
+
+    /**
+     * Sets the initial request time.
+     *
+     * @param initialRequestTime the initial request time
+     * @return the backend web socket handler
+     */
+    public BackendWebSocketHandler setInitialRequestTime(Long initialRequestTime) {
+        this.initialRequestTime = initialRequestTime;
+        return this;
+    }
+
+    /**
+     * Forward to backend.
+     *
+     * @param buffer the buffer
+     */
+    public void forwardToBackend(Buffer buffer) {
+        messages.add(buffer);
+        if (websocket!=null) {
+            writeWebSocket(websocket, messages);
+        }
+    }
+
+    /**
+     * Close websocket.
+     */
+    public void closeWS() {
+        frontendWSisClosed = true;
+        if (! backendWSisClosed) {
+            backendWSisClosed = true;
+            websocket.close();
+        }
+    }
+
+}
---
+++ src/main/java/com/globo/galeb/handlers/FrontendWebSocketHandler.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2014 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.globo.galeb.handlers;
+
+import java.util.Map.Entry;
+
+import com.globo.galeb.entity.impl.Farm;
+import com.globo.galeb.entity.impl.frontend.Virtualhost;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.http.HttpHeaders;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.platform.Container;
+
+/**
+ * Class FrontendWebSocketHandler.
+ *
+ * @author: See AUTHORS file.
+ * @version: 1.0.0, Oct 23, 2014.
+ */
+public class FrontendWebSocketHandler implements Handler<ServerWebSocket> {
+
+//    /** The vertx. */
+//    private final Vertx vertx;
+
+    /** The logger. */
+    private final Logger log;
+
+    /** The farm. */
+    private Farm farm;
+
+    /** The http header host. */
+    private final String httpHeaderHost = HttpHeaders.HOST.toString();
+
+    /**
+     * Instantiates a new frontend web socket handler.
+     *
+     * @param vertx the vertx
+     * @param container the container
+     * @param farm the farm
+     */
+    public FrontendWebSocketHandler(
+            final Vertx vertx,
+            final Container container,
+            final Farm farm) {
+//        this.vertx = vertx;
+        this.farm = farm;
+        this.log = container.logger();
+    }
+
+    /* (non-Javadoc)
+     * @see org.vertx.java.core.Handler#handle(java.lang.Object)
+     */
+    @Override
+    public void handle(final ServerWebSocket serverWebSocket) {
+
+        String hostnameWithPort = "";
+        for (Entry<String, String> e: serverWebSocket.headers().entries()) {
+            if (e.getKey().equalsIgnoreCase(httpHeaderHost)) {
+                hostnameWithPort = e.getValue();
+            }
+        }
+        log.info(String.format("Received request for host %s '%s'",
+                hostnameWithPort, serverWebSocket.uri()));
+
+        if ("".equals(hostnameWithPort)) {
+            log.warn("Host UNDEF");
+            serverWebSocket.close();
+            return;
+        }
+
+        String hostname = hostnameWithPort.split(":")[0];
+
+        final Virtualhost virtualserver = farm.getEntityById(hostname);
+
+        if (virtualserver==null) {
+            log.warn(String.format("Host: %s UNDEF", hostname));
+            serverWebSocket.close();
+            return;
+        }
+
+        if (virtualserver.getEntities().isEmpty()) {
+            log.warn(String.format("Host %s without backends", hostname));
+            serverWebSocket.close();
+            return;
+        }
+
+//        final Backend backend = virtualserver.getChoice(new RequestData(serverWebSocket))
+//                .setKeepAlive(true)
+//                .setKeepAliveTimeOut(Long.MAX_VALUE)
+//                .setKeepAliveMaxRequest(Long.MAX_VALUE)
+//                .setConnectionTimeout(10000)
+//                .setMaxPoolSize(10);
+
+//        String backendId = backend.toString();
+//        log.info(backend);
+//
+//        RemoteUser remoteUser = new RemoteUser(serverWebSocket.remoteAddress());
+//
+//        final HttpClient httpClient = backend.connect(remoteUser);
+//        final BackendWebSocketHandler backendWebSocketHandler =
+//                new BackendWebSocketHandler(vertx, log, backendId, serverWebSocket);
+
+//        httpClient.connectWebsocket(serverWebSocket.uri(),
+//                WebSocketVersion.RFC6455, serverWebSocket.headers(), backendWebSocketHandler);
+//
+//        serverWebSocket.dataHandler(new Handler<Buffer>() {
+//
+//            @Override
+//            public void handle(Buffer buffer) {
+//                backendWebSocketHandler.forwardToBackend(buffer);
+//            }
+//
+//        });
+//
+//        serverWebSocket.closeHandler(new Handler<Void>() {
+//            @Override
+//            public void handle(Void event) {
+//                log.debug("Frontend WebSocket was closed");
+//                backendWebSocketHandler.closeWS();
+//            }
+//        });
+
+    }
+}
