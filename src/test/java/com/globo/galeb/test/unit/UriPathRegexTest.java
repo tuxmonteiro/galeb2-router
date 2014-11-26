26cdfffafeafaaeeaa069d09d35c5fb60d3748fe
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-11-26 21:11:45 -0200

    UriPathRegexTest and UriPathTest added

---
+++ src/test/java/com/globo/galeb/test/unit/UriPathRegexTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2014 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+package com.globo.galeb.test.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.globo.galeb.criteria.ICriterion.CriterionAction;
+import com.globo.galeb.entity.IJsonable;
+import com.globo.galeb.entity.impl.frontend.Rule;
+import com.globo.galeb.entity.impl.frontend.UriPathRegex;
+import com.globo.galeb.entity.impl.frontend.Virtualhost;
+import com.globo.galeb.rulereturn.HttpCode;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.vertx.java.core.http.CaseInsensitiveMultiMap;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonObject;
+
+public class UriPathRegexTest {
+
+    private JsonObject virtualhostJson = new JsonObject().putString(IJsonable.ID_FIELDNAME, "test.localdomain");
+    private Virtualhost virtualhost = new Virtualhost(virtualhostJson);
+    private int numRules = 10;
+    private HttpServerRequest httpServerRequest = null;
+
+    @Before
+    public void setUp() throws Exception {
+
+        virtualhost.start();
+
+        for (Integer x=0; x<numRules; x++) {
+
+            JsonObject ruleJson = new JsonObject().putString(IJsonable.ID_FIELDNAME, x.toString());
+            ruleJson.putObject(IJsonable.PROPERTIES_FIELDNAME, new JsonObject());
+            ruleJson.getObject(IJsonable.PROPERTIES_FIELDNAME)
+                        .putString(Rule.RETURNTYPE_FIELDNAME, HttpCode.class.getSimpleName())
+                        .putString(Rule.MATCH_FIELDNAME, String.format("/%h\\d+.+", x+10));
+            Rule rule = new UriPathRegex(ruleJson).setPriorityOrder(numRules-x);
+
+            virtualhost.addEntity(rule);
+        }
+
+        httpServerRequest = mock(HttpServerRequest.class);
+        try {
+            when(httpServerRequest.absoluteURI()).thenReturn(new URI("null://x/c789xpto"));
+        } catch (URISyntaxException e) {}
+        when(httpServerRequest.headers()).thenReturn(new CaseInsensitiveMultiMap());
+        when(httpServerRequest.params()).thenReturn(new CaseInsensitiveMultiMap());
+        when(httpServerRequest.remoteAddress()).thenReturn(InetSocketAddress.createUnresolved("0.0.0.0", 0));
+
+    }
+
+    @Test
+    public void checkMatch() {
+        Rule rule = virtualhost.getCriterion().when(httpServerRequest).action(CriterionAction.RESET_REQUIRED).thenGetResult();
+        Rule expectedRule = virtualhost.getEntityById("2");
+        assertThat(rule).isEqualTo(expectedRule);
+    }
+
+    @Test
+    public void checkNotMatch() {
+        Rule rule = virtualhost.getCriterion().when(httpServerRequest).action(CriterionAction.RESET_REQUIRED).thenGetResult();
+        Rule expectedRule = virtualhost.getEntityById("5");
+        assertThat(rule).isNotEqualTo(expectedRule);
+    }
+}
